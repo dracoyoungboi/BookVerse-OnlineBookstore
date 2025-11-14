60a17a13e3cf34cfd3303d1f43550104204b08a0
dracoyoungboi <nguyenxuanphananh@gmail.com>
2025-11-14 09:45:06 +0700

    Refactor QRCodeService for improved QR code generation
    - Replaced Hashtable with HashMap for hints in QR code generation.
    - Increased margin setting for QR codes from 1 to 2 for better spacing.
    - Simplified image creation by utilizing MatrixToImageWriter for PNG output, removing manual BufferedImage handling.
    - Streamlined base64 encoding process for QR code images.

--- src/main/java/com/bookverse/BookVerse/service/QRCodeService.java
+++ src/main/java/com/bookverse/BookVerse/service/QRCodeService.java
@@ -3,18 +3,17 @@ package com.bookverse.BookVerse.service;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
 import org.springframework.stereotype.Service;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.util.Base64;
-import java.util.Hashtable;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class QRCodeService {
@@ -29,34 +28,18 @@ public class QRCodeService {
     public String generateQRCodeBase64(String text, int width, int height) {
         try {
             QRCodeWriter qrCodeWriter = new QRCodeWriter();
-            Hashtable<EncodeHintType, Object> hints = new Hashtable<>();
+            Map<EncodeHintType, Object> hints = new HashMap<>();
             hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
             hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
-            hints.put(EncodeHintType.MARGIN, 1);
+            hints.put(EncodeHintType.MARGIN, 2);
 
             BitMatrix bitMatrix = qrCodeWriter.encode(text, BarcodeFormat.QR_CODE, width, height, hints);
 
-            BufferedImage qrImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-            qrImage.createGraphics();
+            ByteArrayOutputStream pngOutputStream = new ByteArrayOutputStream();
+            MatrixToImageWriter.writeToStream(bitMatrix, "PNG", pngOutputStream);
+            byte[] pngData = pngOutputStream.toByteArray();
 
-            Graphics2D graphics = (Graphics2D) qrImage.getGraphics();
-            graphics.setColor(Color.WHITE);
-            graphics.fillRect(0, 0, width, height);
-            graphics.setColor(Color.BLACK);
-
-            for (int i = 0; i < width; i++) {
-                for (int j = 0; j < height; j++) {
-                    if (bitMatrix.get(i, j)) {
-                        graphics.fillRect(i, j, 1, 1);
-                    }
-                }
-            }
-
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            ImageIO.write(qrImage, "PNG", baos);
-            byte[] imageBytes = baos.toByteArray();
-
-            return "data:image/png;base64," + Base64.getEncoder().encodeToString(imageBytes);
+            return "data:image/png;base64," + Base64.getEncoder().encodeToString(pngData);
         } catch (WriterException | IOException e) {
             e.printStackTrace();
             return null;
